13f5b6a56076fe6a1a8ce155717e14d99d880d07
Andrey Lebedev <andrey@lebedev.lt>
2020-05-12 00:02:30 +0300

    Correct type of tmp_path fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Generator
 import asyncio
 
@@ -7,8 +8,8 @@ from ovshell import testing
 
 
 @pytest.fixture()
-def ovshell(tmp_path, event_loop) -> Generator[testing.OpenVarioShellStub, None, None]:
-    ovshell = testing.OpenVarioShellStub(tmp_path)
+def ovshell(tmp_path: Path, event_loop) -> Generator[testing.OpenVarioShellStub, None, None]:
+    ovshell = testing.OpenVarioShellStub(str(tmp_path))
     yield ovshell
     ovshell.stub_teardown()
     event_loop.run_until_complete(asyncio.sleep(0))
