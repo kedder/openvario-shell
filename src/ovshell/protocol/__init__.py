0e330241fc0c74d2021e93fc7bb716534bf6985a
Andrey Lebedev <andrey@lebedev.lt>
2020-07-23 13:07:25 +0300

    Document screen interfaces

--- src/ovshell/protocol/__init__.py
+++ src/ovshell/protocol/__init__.py
@@ -188,12 +188,12 @@ class DeviceManager(Protocol):
 
     @abstractmethod
     def list(self) -> List[Device]:
-        """Enumerate all registred devices"""
+        """Enumerate all registred devices."""
 
     @abstractmethod
     @contextmanager
     def open_nmea(self) -> Generator[NMEAStream, None, None]:
-        """Open new NMEA stream
+        """Open new NMEA stream.
 
         Supposed to be used as a context manager with new NMEAStream object.
         NMEA messages from all the registered devices will be sent to this
@@ -202,7 +202,7 @@ class DeviceManager(Protocol):
 
 
 class ProcessManager(Protocol):
-    """Process Manager
+    """Process Manager.
 
     Wrapper for running asyncio tasks. Keeps a list of active tasks and
     handles task failures.
@@ -214,39 +214,92 @@ class ProcessManager(Protocol):
 
 
 class App(Protocol):
+    """Openvario Shell Application.
+
+    Application represents a runnable item in "Applications" menu of openvario
+    shell.
+
+    `App` objects are created by extensions (see `Extension.list_apps()`) and
+    can run either fully "native" urwid activities inside the shell, or simply
+    run external programs that fully take over the screen.
+    """
+
     name: str
     title: str
     description: str
     priority: int
 
     def install(self, appinfo: "AppInfo") -> None:
-        pass
+        """Install application
+
+        This method is run on openvario shell startup when application is
+        found in one of the extensions for the first time.
+        """
 
     @abstractmethod
     def launch(self) -> None:
-        pass
+        """Start the application"""
 
 
 class Activity(Protocol):
+    """Single view of Application.
+
+    Activity represent one urwid "view" of the application. Activites are
+    organized in a stack by `Screen Manager` and pushed to that stack by
+    the application (becoming active screen seen by the user) and removed from
+    the stack when user presses "Escape" (a.k.a "Cancel" or "Back") button.
+
+    Activity has several lifecycle methods allowing it to react to certain
+    events outside of its direct control.
+    """
+
     @abstractmethod
     def create(self) -> urwid.Widget:
-        pass
+        """Create an urwid widget for this activity.
+
+        The created urwid widget will take all the space reserved for the
+        activity by openvario shell. Typical lifecycle of the activity:
+
+        * activate() - called once
+        * show() - may be called multiple times
+        * hide() - may be called multiple times
+        * destroy() - called once
+
+        Typically it will be the full screen except top and bottom lines, but
+        for modal activities it might be less, depending on modal options.
+        """
 
     def activate(self) -> None:
-        pass
+        """Lifecycle method called when activity is activated for the first
+        time.
+        """
 
     def destroy(self) -> None:
-        pass
+        """Lifecycle method called when activity is removed from the activity
+        stack.
+        """
 
     def hide(self) -> None:
-        pass
+        """Lifecycle method called when activity is being replaced by another
+        activity on top of the activity stack.
+
+        This activity will not receive input until shown again.
+        """
 
     def show(self) -> None:
-        pass
+        """Lifecycle method called when activity is shown for the first time
+        and every time it ends up on top of the activity stack (allowing user
+        to interact with it).
+        """
 
 
 @dataclass
 class ModalOptions:
+    """Options for modal activities.
+
+    Specify the screen area and location of the modal activity.
+    """
+
     align: str
     width: Union[str, int, Tuple[str, int]]
     valign: str
@@ -260,56 +313,121 @@ class ModalOptions:
 
 
 class Dialog(Protocol):
+    """Dialog with several (or no) buttons
+
+    Dialogs are special kinds of modal activities that contain buttons
+    at the bottom. Each button is associated with the handler function,
+    that may return True to close the dialog or False to keep it visible.
+    """
+
     @abstractmethod
     def add_button(self, label: str, handler: Callable[[], bool]) -> None:
-        pass
+        """Add a button to the dialog.
+
+        `handler` is a function that should return True to close the dialog,
+        or False to leave it open.
+        """
 
     @abstractmethod
     def no_buttons(self) -> None:
-        pass
+        """Instructs to make dialog with no buttons
+
+        This can be useful for certain kind of non-interactive dialogs that
+        will close automatically, without explicit user input.
+        """
 
 
 class IndicatorLocation(enum.Enum):
+    """Location of top bar indicator."""
+
     LEFT = "left"
     RIGHT = "right"
 
 
 class ScreenManager(Protocol):
+    """Screen Manager.
+
+    Provides methods for managing activities. Separate UI screens, implemented
+    by activities are organized in a stack. New activities are pushed on
+    top of the stack, making previously active activities "obscured" by it. When
+    user exits the top activity, previous activity is shown again and become
+    interactive.
+
+    Activities may spawn asynchronous tasks, that may run as long as activity
+    is alive.
+    """
+
     @abstractmethod
     def draw(self) -> None:
-        pass
+        """Force screen redraw."""
 
     @abstractmethod
     def push_activity(
         self, activity: Activity, palette: Optional[List[Tuple]] = None
     ) -> None:
-        pass
+        """Push new activity to the activity stack.
+
+        Previous top activity will become hidden, and new activity is shown,
+        until user exits it by using explict UI controls (if any), or by
+        pressing "Excape" button.
+        """
 
     @abstractmethod
     def pop_activity(self) -> None:
-        pass
+        """Explitily destroy the current top activity.
+
+        Previous activity will pop to the top of the stack and will be shown.
+        """
 
     @abstractmethod
     def push_modal(self, activity: Activity, options: ModalOptions) -> None:
-        pass
+        """Push new modal activity.
+
+        Part of the previous' activity UI (that is not covered by area,
+        specified in `options`) will still be visible, but not interactive
+        """
 
     @abstractmethod
     def push_dialog(self, title: str, content: urwid.Widget) -> Dialog:
-        pass
+        """Push special modal activity as a `Dialog`.
+
+        Dialog contents may be any urwid widget, that will be displayed in
+        a box with a title. By default the dialog will have one "Close" button
+        that does nothing other than close the dialog. If `Dialog.add_button()`
+        is called, the default button will be replaced with explicitly set one.
+        More than one button can be added that way.
+        """
 
     @abstractmethod
     def set_indicator(
         self, iid: str, markup: UrwidText, location: IndicatorLocation, weight: int
     ) -> None:
-        pass
+        """Set new indicator (icon on the top bar).
+
+        All indicators must have the unique `iid` string, that is used to change
+        or remove the indicator. Indicator will be visible on screen until is
+        replaced or removed.
+
+        Indicators of the same location are ordered by `weight` value.
+        """
 
     @abstractmethod
     def remove_indicator(self, iid: str) -> None:
-        pass
+        """Remvoe indicator with given `iid`.
+
+        Indicator will be effectively hidden.
+        """
 
     @abstractmethod
     def spawn_task(self, activity: Activity, coro: Coroutine) -> asyncio.Task:
-        pass
+        """Spawn a task for the activity.
+
+        Started task may run as long as activity is alive. When activity is
+        destroyed, all tasks started by that activity are automatically
+        cancelled.
+
+        If task fails, the error will be shown in the status bar.
+        """
 
 
 class OpenVarioOS(Protocol):
