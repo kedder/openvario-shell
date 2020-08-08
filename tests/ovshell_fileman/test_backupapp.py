08281231aec7ac7aa6e2d558c48b9a299668a42e
Andrey Lebedev <andrey@lebedev.lt>
2020-08-08 21:14:24 +0300

    Partial tests for backup app

--- tests/ovshell_fileman/test_backupapp.py
+++ tests/ovshell_fileman/test_backupapp.py
@@ -1,4 +1,14 @@
+import asyncio
+
+import pytest
+import urwid
+
+from ovshell import testing
+
 from ovshell_fileman.backupapp import RsyncStatusLine
+from ovshell_fileman.backupapp import BackupRestoreApp, BackupRestoreMainActivity
+
+from .stubs import AutomountWatcherStub
 
 
 def test_rsync_progress_parse_malformed() -> None:
@@ -42,3 +52,35 @@ def test_rsync_progress_parse2() -> None:
     assert sl.rate == "265.39MB/s"
     assert sl.elapsed == "0:00:05"
     assert sl.xfr is None
+
+
+def test_app_start(ovshell: testing.OpenVarioShellStub) -> None:
+    # GIVEN
+    app = BackupRestoreApp(ovshell)
+
+    # WHEN
+    app.launch()
+
+    # THEN
+    act = ovshell.screen.stub_top_activity()
+    assert isinstance(act, BackupRestoreMainActivity)
+
+
+@pytest.mark.asyncio
+async def test_activity_mounting(ovshell: testing.OpenVarioShellStub) -> None:
+    mountwatcher = AutomountWatcherStub()
+    act = BackupRestoreMainActivity(ovshell, mountwatcher)
+    w = act.create()
+    act.activate()
+    act.show()
+    await asyncio.sleep(0)
+    assert "Please insert USB storage" in _render(w)
+    mountwatcher.stub_mount()
+    await asyncio.sleep(0)
+    assert "This app allows to copy files to and from USB stick" in _render(w)
+
+
+def _render(w: urwid.Widget) -> str:
+    canvas = w.render((60, 40))
+    contents = [t.decode("utf-8") for t in canvas.text]
+    return "\n".join(contents)
