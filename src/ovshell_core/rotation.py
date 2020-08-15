744c35416bb53b83850aedd09a5eec275f08213d
Andrey Lebedev <andrey@lebedev.lt>
2020-08-15 16:52:08 +0300

    Factor out screen rotation mechanics

---
+++ src/ovshell_core/rotation.py
@@ -0,0 +1,40 @@
+from typing import List, Tuple
+
+import re
+import os
+
+from ovshell import api
+
+
+def get_rotations() -> List[Tuple[str, str]]:
+    return [
+        ("0", "Landscape"),
+        ("1", "Portrait (90°)"),
+        ("2", "Landscape (180°)"),
+        ("3", "Portrait (270°)"),
+    ]
+
+
+def apply_rotation(ovos: api.OpenVarioOS, rotation: str) -> None:
+
+    uenvconf_fname = ovos.path("//boot/config.uEnv")
+    if not os.path.exists(uenvconf_fname):
+        ovos.mount_boot()
+
+    with open(uenvconf_fname, "r") as f:
+        uenvconf = f.read()
+
+    uenvconf = re.sub(r"rotation=[0-3]", "rotation=" + rotation, uenvconf)
+
+    with open(uenvconf_fname, "w") as f:
+        f.write(uenvconf)
+
+    # For some weird reason 90 degree rotation is inverted for fbcon
+    fbcon_rotmap = {
+        "0": "0",  # normal
+        "1": "3",  # portrait (90)
+        "2": "2",  # landscape (180)
+        "3": "1",  # portrait (270)
+    }
+    with open(ovos.path("//sys/class/graphics/fbcon/rotate_all"), "w") as f:
+        f.write(fbcon_rotmap[rotation])
--- src/ovshell_core/settings.py
+++ src/ovshell_core/settings.py
@@ -1,10 +1,11 @@
 from typing import Optional, Sequence, Tuple
 import os
-import re
 import subprocess
 
 from ovshell import api
 from ovshell.ui.settings import StaticChoiceSetting
+
+from ovshell_core import rotation
 
 
 class RotationSetting(StaticChoiceSetting):
@@ -24,37 +25,10 @@ class RotationSetting(StaticChoiceSetting):
         self.shell.settings.set(self.config_key, value, save=True)
 
     def get_choices(self) -> Sequence[Tuple[str, str]]:
-        return [
-            ("0", "Landscape"),
-            ("1", "Portrait (90°)"),
-            ("2", "Landscape (180°)"),
-            ("3", "Portrait (270°)"),
-        ]
+        return rotation.get_rotations()
 
-    def _apply_rotation(self, rotation: str) -> None:
-        ovos = self.shell.os
-
-        uenvconf_fname = ovos.path("//boot/config.uEnv")
-        if not os.path.exists(uenvconf_fname):
-            ovos.mount_boot()
-
-        with open(uenvconf_fname, "r") as f:
-            uenvconf = f.read()
-
-        uenvconf = re.sub(r"rotation=[0-3]", "rotation=" + rotation, uenvconf)
-
-        with open(uenvconf_fname, "w") as f:
-            f.write(uenvconf)
-
-        # For some weird reason 90 degree rotation is inverted for fbcon
-        fbcon_rotmap = {
-            "0": "0",  # normal
-            "1": "3",  # portrait (90)
-            "2": "2",  # landscape (180)
-            "3": "1",  # portrait (270)
-        }
-        with open(ovos.path("//sys/class/graphics/fbcon/rotate_all"), "w") as f:
-            f.write(fbcon_rotmap[rotation])
+    def _apply_rotation(self, new_rotation: str) -> None:
+        rotation.apply_rotation(self.shell.os, new_rotation)
 
 
 class LanguageSetting(StaticChoiceSetting):
