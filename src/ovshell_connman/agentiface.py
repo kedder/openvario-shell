28039e56fbf9ecae6a47dc6015d30bb0f5e618ff
Andrey Lebedev <andrey@lebedev.lt>
2020-10-03 20:41:02 +0300

    Add all required methods for net.connman.Agent

--- src/ovshell_connman/agentiface.py
+++ src/ovshell_connman/agentiface.py
@@ -18,10 +18,81 @@ class ConnmanAgentInterface(ServiceInterface):
         self._manager = manager
         self._impl = impl
 
+    @method("Release")
+    def release(self):
+        """This method gets called when the service daemon
+        unregisters the agent. An agent can use it to do
+        cleanup tasks. There is no need to unregister the
+        agent, because when this method gets called it has
+        already been unregistered.
+        """
+        raise NotImplementedError()
+
+    @method("ReportError")
+    def report_error(self, service: "o", error: "s"):  # type: ignore
+        """This method gets called when an error has to be
+        reported to the user.
+
+        A special return value can be used to trigger a
+        retry of the failed transaction.
+
+        Possible Errors: net.connman.Agent.Error.Retry
+        """
+        raise NotImplementedError()
+
+    @method("ReportPeerError")
+    def report_peer_error(peer: "o", error: "s"):  # type: ignore
+        """This method gets called when an error has to be
+        reported to the user about a peer connection.
+
+        A special return value can be used to trigger a
+        retry of the failed transaction.
+
+        Possible Errors: net.connman.Agent.Error.Retry
+        """
+        raise NotImplementedError()
+
+    @method("RequestBrowser")
+    def request_browser(service: "o", url: "s"):  # type: ignore
+        """This method gets called when it is required
+        to ask the user to open a website to proceed
+        with login handling.
+
+        This can happen if connected to a hotspot portal
+        page without WISPr support.
+
+        Possible Errors: net.connman.Agent.Error.Canceled
+        """
+        raise NotImplementedError()
+
     @method("RequestInput")
     async def request_input(
         self, service: "o", fields: "a{sv}",  # type: ignore
     ) -> "a{sv}":  # type: ignore
+        """This method gets called when trying to connect to
+        a service and some extra input is required. For
+        example a passphrase or the name of a hidden network.
+
+        The return value should be a dictionary where the
+        keys are the field names and the values are the
+        actual fields. Alternatively an error indicating that
+        the request got canceled can be returned.
+        OperationAborted will be return on a successful
+        cancel request.
+
+        Most common return field names are "Name" and of
+        course "Passphrase".
+
+        The dictionary arguments contains field names with
+        their input parameters.
+
+        In case of WISPr credentials requests and if the user
+        prefers to login through the browser by himself, agent
+        will have to return a LaunchBrowser error (see below).
+
+        Possible Errors: net.connman.Agent.Error.Canceled
+                 net.connman.Agent.Error.LaunchBrowser
+        """
         print("REQUEST INPUT", service, self._drop_variants(fields))
         svc = await self._manager.get_service(service)
         if svc is None:
@@ -34,6 +105,33 @@ class ConnmanAgentInterface(ServiceInterface):
         raise DBusError("net.connman.Agent.Error.Canceled", f"Not implemented")
 
         return res
+
+    @method("RequestPeerAuthorization")
+    def request_peer_authorization(self, peer: "o", fields: "a{sv}") -> "a{sv}":  # type: ignore
+        """This method gets called when trying to connect to a
+        peer or when an incoming peer connection is requested,
+        for which some extra input is required. In this case,
+        it will only deal with WPS input as well as accepting
+        or rejecting an incoming connection.
+
+        The return value should be a dictionary where the
+        keys are the field names and the values are the
+        actual fields. Alternatively an error indicating that
+        the request got canceled or rejected can be returned.
+
+        The dictionary arguments contains field names with
+        their input parameters.
+
+        Possible Errors: net.connman.Agent.Error.Canceled
+                         net.connman.Agent.Error.Rejected
+        """
+        raise NotImplementedError()
+
+    @method("Cancel")
+    def cancel(self):
+        """This method gets called to indicate that the agent
+        request failed before a reply was returned.
+        """
 
 
 def unpack_variants(var: Union[Variant, Any], tp: SignatureType = None) -> Any:
