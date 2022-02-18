f2ab1dcfc9bd11b57b1de8abd706aea9adc97c69
Andrey Lebedev <andrey@lebedev.lt>
2022-02-19 01:06:19 +0200

    Upgrade black

--- tests/ovshell_fileman_tests/test_utils.py
+++ tests/ovshell_fileman_tests/test_utils.py
@@ -6,5 +6,5 @@ def test_storedfile_format_size() -> None:
     assert format_size(1) == "1.0 B  "
     assert format_size(100000) == "97.7 KiB"
     assert format_size(10000000) == "9.5 MiB"
-    assert format_size(2 ** 32) == "4.0 GiB"
-    assert format_size(2 ** 48) == "256.0 TiB"
+    assert format_size(2**32) == "4.0 GiB"
+    assert format_size(2**48) == "256.0 TiB"
